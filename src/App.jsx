068d3d13c89441654bda480399452147525ae0ef
patryk-pi <patryk.piasecki1@gmail.com>
2023-03-30 21:44:22 +0200

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+import Search from "./components/Search/Search.jsx";
+
+function App() {
+
+  return (
+    <div className="container">
+<Search />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,15 @@
+import {AsyncPaginate} from "react-select-async-paginate";
+
+
+const Search = () => {
+
+    return (
+        <AsyncPaginate
+            placeholder="Search for city"
+            debounceTimeout={}
+            loadOptions={}
+        />
+    )
+}
+
+export default Search;
